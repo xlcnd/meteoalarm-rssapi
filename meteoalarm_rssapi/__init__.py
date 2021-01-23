0bf6f133a6f93b3aeb6e64cdd0448c4a9a4cbfbc
Alexandre Conde <alexandreconde@hotmail.com>
2021-01-23 16:33:00 +0000

    import public resources

--- meteoalarm_rssapi/__init__.py
+++ meteoalarm_rssapi/__init__.py
@@ -5,6 +5,10 @@ from .parser import (
     MeteoAlarmUnrecognizedRegionError,
     MeteoAlarmParseError,
     MeteoAlarmServiceError,
+    awareness_levels,
+    awareness_types,
+    countries,
+    get_regions,
 )
 
 __version__ = "0.1.3"
