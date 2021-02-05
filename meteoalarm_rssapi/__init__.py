efc55290057d8f5b165fa10343c36b3c605614dc
Alexandre Conde <alexandreconde@hotmail.com>
2021-02-05 09:40:52 +0000

    init v0.2.1

--- meteoalarm_rssapi/__init__.py
+++ meteoalarm_rssapi/__init__.py
@@ -13,4 +13,4 @@ from ._resources import (
     countries_list,
 )
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
--- meteoalarm_rssapi/_webquery.py
+++ meteoalarm_rssapi/_webquery.py
@@ -10,7 +10,7 @@ from urllib.error import HTTPError, URLError
 
 from ._exceptions import MeteoAlarmServiceError
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 UA = "meteoalarm_rssapi/{version} (gzip)".format(version=__version__)
