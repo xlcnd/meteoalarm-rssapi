382474befbb6d2364dc27cec313ecf624c9a6d48
Alexandre Conde <alexandreconde@hotmail.com>
2021-02-05 09:12:11 +0000

    add more tests

--- meteoalarm_rssapi/test/test_alerts.py
+++ meteoalarm_rssapi/test/test_alerts.py
@@ -4,13 +4,33 @@
 from meteoalarm_rssapi import MeteoAlarm
 
 
-def test_alerts():
-    """Minimal test for 'alerts'."""
+def test_alerts_coimbra():
+    """Minimal test for 'alerts' (PT COIMBRA)."""
     meteo = MeteoAlarm('PT', 'COIMBRA')
     print(meteo.alerts())
 
 
+def test_alerts_isere():
+    """Minimal test for 'alerts' (FR Isère)."""
+    meteo = MeteoAlarm('FR', 'Isère')
+    print(meteo.alerts())
 
 
+def test_alerts_bergland():
+    """Minimal test for 'alerts' (DE 'Kreis Bautzen - Bergland')."""
+    meteo = MeteoAlarm('DE', 'Kreis Bautzen - Bergland')
+    print(meteo.alerts())
+
+
+def test_alerts_gelderland():
+    """Minimal test for 'alerts' (NL Gelderland)."""
+    meteo = MeteoAlarm('NL', 'Gelderland')
+    print(meteo.alerts())
+
+
+def test_alerts_elhierro():
+    """Minimal test for 'alerts' (ES 'El Hierro')."""
+    meteo = MeteoAlarm('ES', 'El Hierro')
+    print(meteo.alerts())
 
 
