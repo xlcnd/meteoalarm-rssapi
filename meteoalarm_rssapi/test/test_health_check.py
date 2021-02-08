e77a53f9fbba6209bec721c42133abee4174d83f
Alexandre Conde <alexandreconde@hotmail.com>
2021-02-08 16:22:33 +0000

    simplify service's health check

--- meteoalarm_rssapi/test/test_health_check.py
+++ meteoalarm_rssapi/test/test_health_check.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python
 
-import time
-
 from meteoalarm_rssapi import service_health_check
 
 
 def test_service_health_check():
     """Test service (meteoalarm.eu) health check."""
     try:
-        t = time.process_time()
         is_healty = service_health_check()
         assert is_healty
     except ConnectionResetError:
         assert False
-    elapsed_time = time.process_time() - t
-    millis = int(elapsed_time * 1000)
-    assert millis < 1500
 
