2e6a1a1524c906da1a06aa3595122196573805f4
Alexandre Conde <alexandreconde@hotmail.com>
2021-02-08 18:41:18 +0000

    add test health_check (thru class)

--- meteoalarm_rssapi/test/test_health_check.py
+++ meteoalarm_rssapi/test/test_health_check.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
-from meteoalarm_rssapi import service_health_check
+from meteoalarm_rssapi import MeteoAlarm, service_health_check
 
 
-def test_service_health_check():
-    """Test service (meteoalarm.eu) health check."""
+def test_service_health_check_helper():
+    """Test service (meteoalarm.eu) health check (helper)."""
     try:
         is_healty = service_health_check()
+        assert is_healty
+    except ConnectionResetError:
+        assert False
+
+
+def test_service_health_check_class():
+    """Test service (meteoalarm.eu) health check (class)."""
+    try:
+        meteo = MeteoAlarm("PT", "COIMBRA")
+        is_healty = meteo.health_check()
         assert is_healty
     except ConnectionResetError:
         assert False
