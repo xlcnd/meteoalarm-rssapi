b6c51a97fd640e9a622809ddc1915bd6ddc053ef
Alexandre Conde <alexandreconde@hotmail.com>
2021-02-14 10:53:08 +0000

    add 'language' to tests

--- meteoalarm_rssapi/test/test_alerts.py
+++ meteoalarm_rssapi/test/test_alerts.py
@@ -6,29 +6,29 @@ from meteoalarm_rssapi import MeteoAlarm
 
 def test_alerts_coimbra():
     """Minimal test for 'alerts' (PT COIMBRA)."""
-    meteo = MeteoAlarm("PT", "COIMBRA")
+    meteo = MeteoAlarm("PT", "COIMBRA", "pt")
     print(meteo.alerts())
 
 
 def test_alerts_isere():
     """Minimal test for 'alerts' (FR Isère)."""
-    meteo = MeteoAlarm("FR", "Isère")
+    meteo = MeteoAlarm("FR", "Isère", "fr")
     print(meteo.alerts())
 
 
 def test_alerts_bergland():
     """Minimal test for 'alerts' (DE 'Kreis Bautzen - Bergland')."""
-    meteo = MeteoAlarm("DE", "Kreis Bautzen - Bergland")
+    meteo = MeteoAlarm("DE", "Kreis Bautzen - Bergland", "de")
     print(meteo.alerts())
 
 
 def test_alerts_gelderland():
     """Minimal test for 'alerts' (NL Gelderland)."""
-    meteo = MeteoAlarm("NL", "Gelderland")
+    meteo = MeteoAlarm("NL", "Gelderland", "nl")
     print(meteo.alerts())
 
 
 def test_alerts_elhierro():
     """Minimal test for 'alerts' (ES 'El Hierro')."""
-    meteo = MeteoAlarm("ES", "El Hierro")
+    meteo = MeteoAlarm("ES", "El Hierro", "es")
     print(meteo.alerts())
