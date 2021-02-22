c36e2ce9ac141001fcffb4f25e3a45f811facf8a
Alexandre Conde <alexandreconde@hotmail.com>
2021-02-22 23:13:16 +0000

    init v0.3.5

--- meteoalarm_rssapi/__init__.py
+++ meteoalarm_rssapi/__init__.py
@@ -18,4 +18,4 @@ from .exceptions import (
     MeteoAlarmUnrecognizedRegionError,
 )
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
--- meteoalarm_rssapi/_webquery.py
+++ meteoalarm_rssapi/_webquery.py
@@ -8,7 +8,7 @@ from urllib.request import Request, urlopen
 
 from .exceptions import MeteoAlarmServiceError
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 
 UA = "meteoalarm-rssapi/{version} (gzip)".format(version=__version__)
