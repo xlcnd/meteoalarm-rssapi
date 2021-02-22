ac4c93811e4c86cf00786719764b58379788faaa
Alexandre Conde <alexandreconde@hotmail.com>
2021-02-22 09:24:24 +0000

    qa -- fmt

--- meteoalarm_rssapi/test/test_languages.py
+++ meteoalarm_rssapi/test/test_languages.py
@@ -7,9 +7,9 @@ from meteoalarm_rssapi import MeteoAlarm, get_languages
 def test_class_languages():
     """Test 'languages' for country from class."""
     meteo = MeteoAlarm("PT", "COIMBRA")
-    assert ('pt', 'en') == meteo.country_languages()
+    assert ("pt", "en") == meteo.country_languages()
 
 
 def test_get_languages():
     """Test 'languages' for country from call."""
-    assert get_languages("BE") == ('nl', 'fr')
+    assert get_languages("BE") == ("nl", "fr")
