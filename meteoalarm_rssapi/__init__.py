45c64277d6614f342279d5bf238a9dfe60073784
Alexandre Conde <alexandreconde@hotmail.com>
2021-02-23 09:35:58 +0000

    qa -- isort

--- meteoalarm_rssapi/__init__.py
+++ meteoalarm_rssapi/__init__.py
@@ -2,11 +2,11 @@ from ._client import MeteoAlarm
 from ._helpers import countries_iso as country_iso_map
 from ._helpers import get_languages, get_regions, service_health_check, utcnow2iso8601
 from ._resources import (
+    KEYS,
     awareness_levels,
     awareness_types,
     countries_list,
     languages_list,
-    KEYS,
 )
 from .exceptions import (
     MeteoAlarmException,
