cf1bb4fbf552eca598b7e10511c5c76edd0dc4c1
Alexandre Conde <alexandreconde@hotmail.com>
2021-02-24 19:54:18 +0000

    add ISO tests

--- meteoalarm_rssapi/test/test_countries.py
+++ meteoalarm_rssapi/test/test_countries.py
@@ -13,3 +13,9 @@ def test_country_iso_map():
     """Test 'country_iso_map'."""
     print(country_iso_map())
     print(country_iso_map()["Portugal"])
+
+def test_iso3166():
+    """Test countries are ISO 3166-1 Alpha-2."""
+    ISO_3166_1_veted = ('AT','BA','BE','BG','CH','CY','CZ','DE','DK','EE','ES','FI','FR','GR','HR','HU','IE','IL','IS','IT','LT','LU','LV','MD','ME','MK','MT','NL','NO','PL','PT','RO','RS','SE','SI','SK','UK')
+    assert countries_list == ISO_3166_1_veted
+
--- meteoalarm_rssapi/test/test_languages.py
+++ meteoalarm_rssapi/test/test_languages.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-from meteoalarm_rssapi import MeteoAlarm, get_languages
+from meteoalarm_rssapi import MeteoAlarm, get_languages, languages_list
 
 
 def test_class_languages():
@@ -13,3 +13,9 @@ def test_class_languages():
 def test_get_languages():
     """Test 'languages' for country from call."""
     assert get_languages("BE") == ("nl", "fr")
+
+
+def test_iso_languages():
+    """Test 'languages' are  ISO 639-1."""
+    ISO_639_1_veted = ('bg','bs','cs','de','dk','el','en','es','et','fi','fr','he','hr','hu','is','it','lt','lv','mk','mt','nl','no','pl','pt','ro','sk','sl','sr','sv')
+    assert languages_list == ISO_639_1_veted
