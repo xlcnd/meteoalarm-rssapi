67cc312e0ed4a2213c73a814cf083b7bdcbb9a2f
Alexandre Conde <alexandreconde@hotmail.com>
2021-03-17 09:06:23 +0000

    add docstrings

--- meteoalarm_rssapi/_client.py
+++ meteoalarm_rssapi/_client.py
@@ -1,3 +1,5 @@
+"""Main class to interact with the 'meteoalarm' service."""
+
 from ._helpers import get_languages, get_regions, service_health_check
 from ._parser import parser
 from ._resources import awareness_levels, awareness_types
@@ -15,6 +17,8 @@ TIMEOUT = 20
 
 
 class MeteoAlarm:
+    """Main class to interact with the 'meteoalarm' service."""
+
     def __init__(self, country, region, language=None, timeout=TIMEOUT):
         try:
             country = country.upper()
@@ -40,26 +44,29 @@ class MeteoAlarm:
 
     @staticmethod
     def countries():
+        """Return the list of the participating countries (ISO 3166-1 Alpha-2)."""
         return _countries
 
     @staticmethod
-    def awareness_levels():
-        return awareness_levels
-
-    @staticmethod
     def awareness_types():
+        """Return the list of 'awareness types' (events)."""
         return awareness_types
 
-    def health_check(self, timeout=2):
-        return service_health_check(self._url, timeout)
+    @staticmethod
+    def awareness_levels():
+        """Return the list of 'awareness levels' (severity)."""
+        return awareness_levels
 
     def country_regions(self):
+        """Return the list of 'regions' for the country."""
         return get_regions(self._country)
 
     def country_languages(self):
+        """Return the list of 'languages' for the country."""
         return get_languages(self._country)
 
     def alerts(self):
+        """Return the list of 'alerts' currently available."""
         try:
             tmfast = int(0.3 * self._timeout)
             data = query(self._url, timeout=tmfast) or ""
@@ -72,3 +79,7 @@ class MeteoAlarm:
         except MeteoAlarmServiceError:
             raise MeteoAlarmServiceError()
         return parser(rss, self._country, self._region, self._lang)
+
+    def health_check(self, timeout=2):
+        """Verify if the server is responding."""
+        return service_health_check(self._url, timeout)
